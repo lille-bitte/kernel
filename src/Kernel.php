8b3174af04ac8f25ad4011733b6cefad34f56356
Paulus Gandung <gandung@slackerc0de.us>
2020-02-23 20:44:05 +0700

    Add LogicException to use class list.

--- src/Kernel.php
+++ src/Kernel.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace LilleBitte\Kernel;
 
+use LogicException;
 use ReflectionClass;
 use LilleBitte\Annotations\ClassRegistry;
 use LilleBitte\Annotations\AnnotationReader;
