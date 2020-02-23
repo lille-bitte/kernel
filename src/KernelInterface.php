ebe7fe36c3f13d362970601dd98ebe10b044516e
Paulus Gandung <gandung@slackerc0de.us>
2020-02-23 21:17:19 +0700

    Adding prototype method handle()  in src/KernelInterface.php

--- src/KernelInterface.php
+++ src/KernelInterface.php
@@ -4,11 +4,21 @@ declare(strict_types=1);
 
 namespace LilleBitte\Kernel;
 
+use Psr\Http\Message\RequestInterface;
+
 /**
  * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
  */
 interface KernelInterface
 {
+	/**
+	 * Handle request object.
+	 *
+	 * @param RequestInterface $request Request object.
+	 * @return void
+	 */
+	public function handle(RequestInterface $request);
+
 	/**
 	 * Register given extensions.
 	 *
