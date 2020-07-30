4101b50fae17d32e985e3671fa12262dafaebfde
plvhx <gandung@slackerc0de.us>
2020-07-30 20:11:30 +0700

    Refactoring src/KernelInterface.php

--- src/KernelInterface.php
+++ src/KernelInterface.php
@@ -25,12 +25,4 @@ interface KernelInterface
      * @return iterable
      */
     public function registerExtensions(): iterable;
-
-    /**
-     * Register given annotation classes.
-     *
-     * @param array $classes List of classes.
-     * @return void
-     */
-    public function registerAnnotationClasses(array $classes);
 }
